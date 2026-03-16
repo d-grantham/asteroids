0aa882f6417f818226a211d92b350800b96e4e36
Derik Grantham <derikwg05@gmail.com>
2026-03-16 13:10:10 -0600

    previous commit, missed some files

--- main.py
+++ main.py
@@ -3,23 +3,17 @@ from constants import *
 from logger import log_state
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
         log_state()
-        
         for event in pygame.event.get():
-            pass
+            if event_type == pygame.QUIT():
+                return
 
         screen.fill("black")
-        
-        
         pygame.display.flip()
 if __name__ == "__main__":
     main()
