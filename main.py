684cfcb9859b87c41795981ed1be16986143a508
Derik Grantham <derikwg05@gmail.com>
2026-03-16 13:44:03 -0600

    add player.py and draw to screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     
@@ -8,6 +9,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     
     while True:
         log_state()
@@ -18,7 +21,11 @@ def main():
         dt = clock.tick(60) / 1000
         
         screen.fill("black")
+        
+        player.draw(screen)
+        
         pygame.display.flip()
+        
         
 
 
